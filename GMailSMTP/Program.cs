3d6675087bdd84321442502c7da6141fa1427b27
Deena Nath <deny.inbox@gmail.com>
2020-05-08 19:11:25 +0530

    Add project files.

---
+++ GMailSMTP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GMailSMTP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
